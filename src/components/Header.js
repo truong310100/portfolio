7fb5aa57f4b9f7dd4a1adc7b58e7ace616f19e5e
truong310100 <truong310100@gmail.com>
2025-04-07 20:15:40 +0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { href } from "react-router-dom";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -21,11 +20,13 @@ const Header = () => {
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
+  const closeMenu = () => setIsMenuOpen(false);  // Hàm đóng menu khi click vào một liên kết
+
   return (
     <header className="bg-blue-600 text-white p-4 fixed w-full top-0 left-0 z-10">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
-      <a href="#summary" className="text-3xl font-bold border-t border-b cursor-pointer">NLT</a>
-      
+        <a href="#summary" className="text-3xl font-bold border-t border-b cursor-pointer">NLT</a>
+        
         {/* Menu for larger screens */}
         <nav className="hidden md:flex">
           <a href="#summary" className="hover:underline underline-offset-8 px-4">Summary</a>
@@ -47,11 +48,11 @@ const Header = () => {
       {/* Mobile Menu */}
       <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
         <nav className="flex flex-col items-end bg-blue-600 py-4">
-          <a href="#summary" className="hover:underline underline-offset-8 px-4 py-2">Summary</a>
-          <a href="#skills" className="hover:underline underline-offset-8 px-4 py-2">Skills</a>
-          <a href="#working" className="hover:underline underline-offset-8 px-4 py-2">Working</a>
-          <a href="#education" className="hover:underline underline-offset-8 px-4 py-2">Education</a>
-          <a href="#contact" className="hover:underline underline-offset-8 px-4 py-2">Contact</a>
+          <a href="#summary" onClick={closeMenu} className="hover:underline underline-offset-8 px-4 py-2">Summary</a>
+          <a href="#skills" onClick={closeMenu} className="hover:underline underline-offset-8 px-4 py-2">Skills</a>
+          <a href="#working" onClick={closeMenu} className="hover:underline underline-offset-8 px-4 py-2">Working</a>
+          <a href="#education" onClick={closeMenu} className="hover:underline underline-offset-8 px-4 py-2">Education</a>
+          <a href="#contact" onClick={closeMenu} className="hover:underline underline-offset-8 px-4 py-2">Contact</a>
         </nav>
       </div>
     </header>
